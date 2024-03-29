5803c769bc62a40bdebd080eae91fd0f8e073442
diogocastros1 <diogoacsantos93@gmail.com>
2024-03-29 10:43:53 -0300

    feat: :sparkles: Adding user interface to enter information

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,45 @@
+import * as readline from 'readline';
 import { CurrencyConverter } from './services/interfaces/CurrencyConverterServices';
 import { ExchangeRateAPI } from './services/currencyConverter';
 
 class CurrencyConverterApp {
   private converter: CurrencyConverter;
+  private rl: readline.Interface;
 
   constructor(converter: CurrencyConverter) {
     this.converter = converter;
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    })
   }
 
   async run() {
     try {
-      const amount = 10;
-      const from = 'USD';
-      const to = 'EUR';
-      const result = await this.converter.convert(amount, from, to);
-      console.log(`${amount} ${from} equals ${result} ${to}`);
-    } catch (e) {
-      console.error(e.message);
+      this.rl.question('Entre com o valor que deseja converter: ', async (answer) => {
+        const amount = parseFloat(answer);
+        if(isNaN(amount)) {
+          console.log('Por favor, insira um valor numérico');
+          this.run();
+          return;
+        }
+        this.rl.question('Qual a moeda do valor digitado? ', async(from) => {
+          this.rl.question('Para qual moeda deseja converter? ', async(to) => {
+          try {
+            const result = await this.converter.convert(amount, from.toUpperCase(), to.toUpperCase());
+            console.log(`${amount.toFixed(2)} ${from.toUpperCase()} eh igual a ${result.toFixed(2)} ${to.toUpperCase()}`);
+            this.rl.close();
+            
+          } catch (error) {
+            console.log(error);
+          }
+          })
+        })
+        
+      })
+            
+    } catch (error) {
+      console.error(error);
     }
   }
 }
